ebe691d35f1f23ca0f993fd4eb799881f3340cef
Lindah Nyambura <l.nyambura@alustudent.com>
2023-07-27 18:30:04 -0700

    moja

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square by"""
+
+
 class Square:
-    """
-    Represents a square.
-
-    A square is a geometrical shape with four equal sides and four right angles.
-    Attributes:
-        __size (int): The size of each side of the square (private).
-
-    Methods:
-        __init__(self, size): Initializes a new Square object with the given size.
-
-    """
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification)"""
     def __init__(self, size):
-        """
-        Initializes a new Square object.
-
-        Args:
-            size (int): The size of each side of the square.
-
-        """
         self.__size = size
-
