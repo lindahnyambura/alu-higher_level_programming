6999c7009ef949d683e1154d105ffa8c48821eb3
Lindah Nyambura <l.nyambura@alustudent.com>
2023-07-27 18:45:44 -0700

    mbili

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
+"""Write a class Square that defines a square"""
+
+
 class Square:
-    """
-    Represents a square.
-    Attributes:
-        __size (int): The size of each side of the square (private).
+    """Write a class Square that defines a square"""
 
-    Methods:
-        __init__(self, size=0): Initializes a new Square object with the given size.
-
-    """
     def __init__(self, size=0):
-        """
-        Initializes a new Square object.
-
-        Args:
-            size (int, optional): The size of each side of the square. Defaults to 0.
-
-        Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is less than 0.
-
-        """
-        if not isinstance(size, int):
+        """initialize square
+               Args:
+                   size (int): size of the square
+               """
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-
+        else:
+            self.__size = size
