9888c1c5120913db00bf2fc91f641a256b57eb74
Lindah Nyambura <l.nyambura@alustudent.com>
2023-08-17 07:04:14 -0700

    wewe

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+ script that lists all states with a name starting with N from the database hbtn_0e_0_usa
 """
 import MySQLdb
 import sys
@@ -14,7 +14,7 @@ if __name__ == '__main__':
             db=sys.argv[3],
             port=3306
             )
-    cur=db.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
             ORDER BY states.id ASC")
     table = cur.fetchall()
