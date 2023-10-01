ac039e31ec9760c58d34e7d62df8556f69eb972c
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-01 14:36:35 +0200

    lint

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const c = 'C is fun'
-const python = 'Python is cool'
-const javascript = 'JavaScript is amazing'
+const c = 'C is fun';
+const python = 'Python is cool';
+const javascript = 'JavaScript is amazing';
 
-console.log(c)
-console.log(python)
-console.log(javascript)
+console.log(c);
+console.log(python);
+console.log(javascript);
