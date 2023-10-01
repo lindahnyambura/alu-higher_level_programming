067ff7b605ebc268f83b65ab28b897a95d67e8e7
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-01 15:13:12 +0200

    lint

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
     }
   }
 
-  print(c = 'X') {
+  print (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
         process.stdout.write(c);
@@ -16,11 +16,11 @@ class Rectangle {
     }
   }
 
-  rotate() {
+  rotate () {
     [this.width, this.height] = [this.height, this.width];
   }
 
-  double() {
+  double () {
     [this.width, this.height] = [this.width * 2, this.height * 2];
   }
 }
