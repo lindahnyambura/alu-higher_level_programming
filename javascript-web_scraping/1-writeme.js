3926b02cb7b5e0f212281f4543bfc6903ed9f49c
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 00:26:38 -0700

    filewrite

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const filePath = process.argv[2];
+const data = process.argv[3];
+
+fs.writeFile(filePath, data, 'utf8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+});
