f453d078e3846ed402279bf72de62add1e729384
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 07:54:04 +0200

    readFile

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+var fs = require('fs');
+
+const filePath = process.argv[2];
+
+fs.readFile(filePath, function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    console.log(data.toString());
+});
---
+++ javascript-web_scraping/README.md
@@ -0,0 +1,1 @@
+This is a project on Web Scraping in JavaScript
