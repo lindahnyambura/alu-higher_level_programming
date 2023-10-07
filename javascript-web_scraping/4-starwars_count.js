fe8474f37ecdce5b0c1d50c8029d8e0fd384ef9d
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 08:53:11 -0700

    count

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    return console.log(error);
+    }
+
+    const movieData = JSON.parse(body);
+    let count = 0;
+
+    for (const movie of movieData.results) {
+        if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+            count++;
+        }
+    }
+    console.log(count);
+});
