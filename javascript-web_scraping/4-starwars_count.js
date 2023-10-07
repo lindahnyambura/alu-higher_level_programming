1de46695cca8c7e039715b76eeaa0b52a54e4782
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 09:10:36 -0700

    indentation

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,15 @@ const apiUrl = process.argv[2];
 request(apiUrl, (error, response, body) => {
   if (error) {
     return console.log(error);
-    }
+  }
 
-    const movieData = JSON.parse(body);
-    let count = 0;
+  const movieData = JSON.parse(body);
+  let count = 0;
 
-    for (const movie of movieData.results) {
-        if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-            count++;
-        }
+  for (const movie of movieData.results) {
+    if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+      count++;
     }
-    console.log(count);
+  }
+  console.log(count);
 });
