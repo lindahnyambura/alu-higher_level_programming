c06dfae2cff309370e4b06d1d412e3e3f241cb7b
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 01:56:41 -0700

    trailing space correction

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@ const request = require('request');
 const url = process.argv[2];
 request(url, (error, response) => {
   if (error) {
-    return console.log(error); 
+    return console.log(error);
   }
   console.log(`code: ${response.statusCode}`);
 });
