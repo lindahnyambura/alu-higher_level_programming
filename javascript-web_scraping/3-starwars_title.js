f127ac0a23aa9e64b22dddb37e62fc14ac04a099
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 07:10:51 -0700

    lint

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -6,7 +6,7 @@ const starwarsUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request(starwarsUrl, (error, response, body) => {
   if (error) {
     return console.log(error);
-    }
-    const movieData = JSON.parse(body);
-    console.log(movieData.title);
+  }
+  const movieData = JSON.parse(body);
+  console.log(movieData.title);
 });
