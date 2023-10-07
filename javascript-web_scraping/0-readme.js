ef9a7964adff9e630c96c8763818dc5baaf9abf9
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-06 23:47:55 -0700

    lint

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
-fs.readFile(filePath, function(err, data) {
-    if (err) {
-        return console.log(err);
-    }
-    console.log(data.toString());
+fs.readFile(filePath, function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data.toString());
 });
