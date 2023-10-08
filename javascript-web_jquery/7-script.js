aa6aaeabe65a3c3a92cc71c155810f4d574afd5b
Lindah Nyambura <l.nyambura@alustudent.com>
2023-10-07 19:24:38 -0700

    character

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+
+  $.getJSON(url, function(data) {
+    $('#character').text(data.name);
+  });
+});
